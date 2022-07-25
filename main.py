662b59d7a1175bd87d95a8c0b636e0c58776afcf
Arin6145 <108275177+Arin6145@users.noreply.github.com>
2022-07-25 18:33:17 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,50 +1,42 @@
 import discord, os
-from discord.ext import commands
-from discord.ui import Button, View, Select
+from discord import app_commands
 
-INTENTS = discord.Intents.all()
-bot = commands.Bot(command_prefix="!", intents=INTENTS)
 token = os.getenv("TOKEN")
 
-async def SendError(self, ctx:discord.ApplicationContext, message):
-        embed = discord.Embed(title=f'실패 Failed # {ctx.command}', description=f'{message}', color=self.color)
-        await ctx.respond(embed=embed)
-
-@bot.slash_command(name='인증', description='서포트 서버 인증을 시도합니다.')
-async def CaptchaSystem(self, ctx:discord.ApplicationContext):
-    role = ctx.guild.get_role(988011044817485824)
-    a = ""
-    Captcha_img = ImageCaptcha()
-    if role not in ctx.author.roles:
-        for i in range(6):
-            a += str(random.randint(0, 9))
-
-        name = './Verifty/' + str(ctx.author. id) + ".png"
-        Captcha_img.write(a, name)
-
-        embed = discord.Embed(title=f'대기중 Waited # {ctx.command}', description=f'숫자를 채팅으로 입력해주세요.', color=self.color)
-        embed.add_field(name='이용 약관', value=f'✔ㅣ이용약관 (필독)',inline=False)
-
-        edit_msg = await ctx.respond(file=discord.File(name), embed=embed)
-
-        def check(m: discord.Message):  # m = discord.Message.
-            return m.author == ctx.author and m.channel.id == ctx.channel.id 
-
-        try:
-            msg = await self.bot.wait_for('message', check=check, timeout=15)
-
-        except asyncio.TimeoutError:
-            await self.SendError(ctx, '인증에 실패했습니다. 시간이 초과되었습니다.')
-        else:
-            if msg.content == a:
-                embed = discord.Embed(title=f'성공 Success', description=f'{ctx.author}님의 인증에 성공했습니다.', color=self.color)
-                embed.add_field(name='지급 역할', value=f'<@&{role.id}> (2초 후 자동 지급)', inline=False)
-                await edit_msg.edit_original_message(embed=embed)
-                await asyncio.sleep(2)
-                await ctx.author.add_roles(role)
-            else:
-                await self.SendError(ctx, '인증에 실패했습니다. 잘못 입력하셨습니다.')
-    else:
-        pass
+class button_view(discord.ui.View):
+    def __init__(self) -> None:
+        super().__init__(timeout=None)
     
-bot.run(token)
+    @discord.ui.button(label = "인증하기", style = discord.ButtonStyle.green, custom_id = "role_button")
+    async def verify(self, interaction: discord.Interaction, button: discord.ui.Button):
+        if type(client.role) is not discord.Role: client.role = interaction.guild.get_role(988011044817485824)
+        if client.role not in interaction.user.roles:
+            await interaction.user.add_roles(client.role)
+            await interaction.response.send_message(f"{client.role.mention}역할이 지급되었습니다!", ephemeral = True)
+        else: await interaction.response.send_message(f"이미 {client.role.mention}역할을 가지고 있습니다!", ephemeral = True)
+
+class aclient(discord.Client):
+    def __init__(self):
+        super().__init__(intents = discord.Intents.default())
+        self.synced = False #we use this so the bot doesn't sync commands more than once
+        self.added = False
+        self.role = 988011044817485824
+
+    async def on_ready(self):
+        await self.wait_until_ready()
+        if not self.synced: #check if slash commands have been synced 
+            await tree.sync(guild = discord.Object(id=988009947684012092)) #guild specific: leave blank if global (global registration can take 1-24 hours) - sync only when you have to
+            self.synced = True
+        if not self.added:
+            self.add_view(button_view())
+            self.added = True
+        print(f"We have logged in as {self.user}.")
+
+client = aclient()
+tree = app_commands.CommandTree(client)
+
+@tree.command(guild = discord.Object(id=988009947684012092), name = 'button', description='Launches a button!') #guild specific slash command
+async def launch_button(interaction: discord.Interaction): 
+    await interaction.response.send_message(view = button_view())
+
+client.run(token)
